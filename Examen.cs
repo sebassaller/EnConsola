8a8cb93a61b0cad1356446323aa308e665fcb128
sebastian saller <72841007+sebassaller@users.noreply.github.com>
2023-06-16 10:28:14 -0300

    agregando primer commit
    subiendo proyecto

---
+++ Examen.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace examendbtglobal
+{
+    public class Examen
+    {
+        public  int[] ConvertirArray(string foo)
+        {
+            string[] tokens = foo.Split(",");
+            List<int> nums = new List<int>();
+            int oneNum;
+            foreach (string s in tokens)
+            {
+                if (Int32.TryParse(s, out oneNum))
+                    nums.Add(oneNum);
+            }
+            return nums.ToArray();
+        }
+
+
+        public int Ocurrecnias(int buscador, int[] array)
+        {
+            int contador = 0;
+            foreach (int i in array)
+            {
+                if (i == buscador)
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+        public void tema2(int[] array)
+        {
+
+            Array.Sort(array);
+            var ListaArray = array.GroupBy(x => x).Where(g => g.Count() > 0).Select(y => new { item = y.Key, count = y.Count() }).ToList();
+            for (int ii = 0; ii < 10; ii++)
+            {
+                var validar = ListaArray.GroupBy(x => x).Where(g => g.Key.item == ii).Any();
+
+                if (validar==false)
+                {
+                    ListaArray.Insert(ii, new { item = ii, count = 0 });
+                }
+                if (ListaArray[ii].item > 0)
+                {
+                    Console.WriteLine(ListaArray[ii].item + ":" + Asteriscos(ListaArray[ii].count));
+                }
+            }
+        }
+       public static string Asteriscos(int Entero)
+        {
+            string resul = "";
+            if (Entero > 0)
+            {
+                for (int n = 1; n <= Entero; n++)
+                {
+                    resul = resul + "*";
+                }
+            }
+            return resul;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace examendbtglobal
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            try {
+
+                Console.WriteLine("Bienvenidos examen dbtglobal");
+
+                Console.WriteLine("Ingrese el numero  al temario del examen 1:Ocurrecnias,2:Lista Asteriscos ");
+                int Respuesta = int.Parse(Console.ReadLine());
+                var examen = new Examen(); 
+                Console.WriteLine("ingrese los numeros separados por coma");
+                
+                var array = examen.ConvertirArray(Console.ReadLine().ToString());
+
+                if (Respuesta == 1)
+                {
+                    Console.WriteLine("Ingrese el numero  al que decea buscar en el array");
+                    int numerobuscador = int.Parse(Console.ReadLine());
+                    Console.WriteLine("El numero que ingreso coincide:" + examen.Ocurrecnias(numerobuscador, array) + " veces");
+                }
+                if (Respuesta == 2)
+                {
+                    //int[] myArray = { 8, 1, 6, 7, 8, 4, 9, 8, 2 };este era el ejemplo en word
+
+                    var validarango = array.GroupBy(x => x).Where(g => g.Key > 9).Count();
+
+                    if (validarango != 0)
+                    {
+                        Console.WriteLine("Elementos fuera de Rangos 1-9");
+                    }
+                    else
+                    {
+                        examen.tema2(array);
+                    }
+                }
+
+            } catch (Exception ex) {
+
+                Console.WriteLine("Error inesperado", ex.Message);
+                Console.WriteLine("Compruebe que los datos sean correctos");
+            }
+            
+        }
+    }
+}
